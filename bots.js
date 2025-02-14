8c47a99912b131ddbd8813bfa18e03a559df5c72
korimiku <daddadlif10@gmail.com>
2025-02-14 17:41:02 +0300

    micro test

--- bots.js
+++ bots.js
@@ -7,10 +7,10 @@ console.log("Запуск Бота")
 
 const bot = mineflayer.createBot({
   
-  host: 'localhost', // minecraft server ip
+  host: 'altair.minerent.net', // minecraft server ip
   username: 'KoriBotTest', // username to join as if auth is `offline`, else a unique identifier for this account. Switch if you want to change accounts
   auth: 'offline', // for offline mode servers, you can set this to 'offline'
-  port: 25565,
+  port: 25600,
   // port: 25565,              // set if you need a port that isn't 25565
   // version: false,           // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
   // password: '12345678'      // set if you want to use password-based auth (may be unreliable). If specified, the `username` must be an email
@@ -28,6 +28,9 @@ bot.on('chat', (username, message) => {
   if (message.toLowerCase() === "твое хп" ) {
     bot.chat(Math.round(bot.health));
   }
+
+  
+  
 
   if (message.toLowerCase() === "игроки максимум" ) {
     bot.chat(bot.game.maxPlayers);
@@ -68,8 +71,6 @@ bot.on('path_update', (result) => {
 bot.on('goal_reached', () => {
   bot.chat('Я на месте!');
 });
-
-
 
 
 
