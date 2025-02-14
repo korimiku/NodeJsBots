4dba561629ddc02cd69720782e9c5a7586108924
korimiku <daddadlif10@gmail.com>
2025-02-14 18:16:21 +0300

    Опыт обхода регистрации

--- bots.js
+++ bots.js
@@ -8,9 +8,10 @@ console.log("Запуск Бота")
 const bot = mineflayer.createBot({
   
   host: 'altair.minerent.net', // minecraft server ip
-  username: 'KoriBotTest', // username to join as if auth is `offline`, else a unique identifier for this account. Switch if you want to change accounts
+  username: 'KoriLol12', // username to join as if auth is `offline`, else a unique identifier for this account. Switch if you want to change accounts
   auth: 'offline', // for offline mode servers, you can set this to 'offline'
   port: 25600,
+  version: "1.12.2"
   // port: 25565,              // set if you need a port that isn't 25565
   // version: false,           // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
   // password: '12345678'      // set if you want to use password-based auth (may be unreliable). If specified, the `username` must be an email
@@ -23,16 +24,14 @@ bot.on('chat', (username, message) => {
   if (username === bot.username) return
   if (message.toLowerCase() === "привет" ) {
     bot.chat("Привет! вот мои доступные команды:\n 1)'Твое хп'\n 2) Укажите координаты в виде x:n y:n z:n p:n, p - погрешность в блоках, и я приду на них! Чтобы остановить меня скажите: 'Не иди' ");
-    bot.chat("3)игроки максимум");
+    bot.chat("3)игроки макс");
   }
   if (message.toLowerCase() === "твое хп" ) {
     bot.chat(Math.round(bot.health));
   }
 
-  
-  
 
-  if (message.toLowerCase() === "игроки максимум" ) {
+  if (message.toLowerCase() === "игроки макс" ) {
     bot.chat(bot.game.maxPlayers);
   }
 
@@ -76,7 +75,15 @@ bot.on('goal_reached', () => {
 
 
 bot.on('spawn', () => {
-    console.log("Бот заспавнился")
+    console.log("Бот заспавнился");
+    bot.chat('/l iopqwe1');
+    bot.setControlState('forward', true);
+
+    // Останавливаем бота через 5 блоков
+    setTimeout(() => {
+      bot.setControlState('forward', false); // Останавливаем движение
+      bot.chat("Анти Бот Пройден!");
+    }, 5000); // Время в миллисекундах (5 секунд для 5 блоков)
 });
 
 
